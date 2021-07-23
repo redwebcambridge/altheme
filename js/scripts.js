745692a0fdfe38b3afc3aeeb28f99f40ad11acdc
Lewis Cornwell <lewis@redgraphic.co.uk>
2021-07-23 18:12:02 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function() {
   (function() {
     if (!localStorage.getItem('cookie-consent')) {
       console.log('no cookie consent set');
-      jQuery('html').css('display','flex!important');
+      jQuery('.cookie-consent').css('display','flex!important');
       document.querySelector('.allow-button').onclick = function(e) {
         e.preventDefault();
         document.querySelector('.cookie-consent').style.display = 'none';

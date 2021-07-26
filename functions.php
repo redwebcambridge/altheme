c16ddd8b196d9e09d2e9915002f9fdac1a523e8b
Lewis Cornwell <lewis@redgraphic.co.uk>
2021-07-26 15:50:26 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ function anglian_learning_scripts() {
   wp_enqueue_script( 'al-gallery-js', 'https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js');
   wp_enqueue_style( 'al-gallery-style', get_template_directory_uri().'/lib/ekko-lightbox.css');
   //Custom scripts
-  wp_enqueue_script( 'al-scripts', get_template_directory_uri().'/js/scripts.js');
+  wp_enqueue_script( 'al-scripts', get_template_directory_uri().'/js/scripts.js', array( 'jquery' ), '1.0.0', true );
  
 }
 add_action( 'wp_enqueue_scripts', 'anglian_learning_scripts' );

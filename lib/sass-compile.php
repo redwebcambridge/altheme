c6e8977d42c58608ae502817784f4faf2a988ef3
Lewis Cornwell <lewis@lewiscornwell.co.uk>
2021-09-06 16:51:36 +0100

    Theme update

--- lib/sass-compile.php
+++ lib/sass-compile.php
@@ -1,10 +1,4 @@
 <?php
-//Clear Cache
-header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
 //Main colours
   $logos = get_field('logo_and_icons','option');
   $colours = get_field('colours','option');

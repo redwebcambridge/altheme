7c39c19bd778f0696c1aff751076989344d36e9f
DavidPresto <david@redgraphic.co.uk>
2021-10-15 13:06:52 +0100

    Neil changes

--- page-templates/meet-the-staff-with-picture.php
+++ page-templates/meet-the-staff-with-picture.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
           <!---Body section--->
           <div class="col-md-9">
-            
+
               <div class="row">
                 <div class="col">
                   <h2 class="heading-two"><?php the_field('sub_heading') ?></h2>
@@ -27,44 +27,57 @@ get_header(); ?>
               </div>
 
               <div class="row mb-5">
-                <div class="col-12 staff-container">
+
                     <?php if( have_rows('table_of_staff') ): while( have_rows('table_of_staff') ): the_row(); ?>
-                      <div class="staff-section">
 
-                        <?php if (get_sub_field('picture')): ?>
-                          <div class="thumbnail" style="background-image:url('<?php the_sub_field('picture'); ?>')"></div>
-                          <div class="member-block-image-ghost"></div>
-                        <?php endif; ?>                          
-                        
-                        <!-- <div class="staff-text"> -->
-                          <h3><?php the_sub_field('name'); ?></h3>
-                          <p>
-                          <?php
-                          if(get_sub_field('role')){echo '<strong>'.get_sub_field('role').'</strong>';}
-                          if(get_sub_field('department')){echo ', '.get_sub_field('department');}
-                          if(get_sub_field('form__mentor_group')){echo ', <em>'.get_sub_field('form__mentor_group').'</em>';} 
-                          if(get_sub_field('email')){echo '<br><a href="mailto:'.get_sub_field('email').'">'.get_sub_field('email')."</a>";}
-                          ?>
-                          </p>
-                          <?php 
-                          $content = substr(get_sub_field('description'),0,80);
-                          $content = substr($content,0,strrpos($content,' '));
-                          $$content = preg_replace('/<span[^>]+\>/i', '', $content); 
-                          echo '<div class="desc_short">'.$content."...</div>";
-                          ?>
+                        <div class="col-12 col-lg-6 staff-container">
+                            <div class="staff-section">
+                                <div class="row">
 
-                          <div class="allcontent"><?php echo get_sub_field('description'); ?></div>
-                          <p class="read-more-button">More</p>
+                                    <?php if (get_sub_field('picture')): ?>
+                                    <div class="col-12 col-sm-6 col-md-4 col-lg-5 staffimage" style="background-image:url('<?php the_sub_field('picture'); ?>')">
+                                        <div class="member-block-image-ghost"></div>
+                                    </div>
+                                    <?php endif; ?>
 
-                      </div>
+                                    <?php if (get_sub_field('picture')) { ?>
+                                    <div class="col-12 col-sm-6 col-md-8 col-lg-7">
+                                    <?php } else { ?>
+                                    <div class="col-12">
+                                    <?php } ?>
+                                        <div class="staffdetails">
+                                            <h3><?php the_sub_field('name'); ?></h3>
+                                            <p>
+                                            <?php
+                                            if(get_sub_field('role')){echo '<strong>'.get_sub_field('role').'</strong>';}
+                                            if(get_sub_field('department')){echo ', '.get_sub_field('department');}
+                                            if(get_sub_field('form__mentor_group')){echo ', <em>'.get_sub_field('form__mentor_group').'</em>';}
+                                            if(get_sub_field('email')){echo '<br><a href="mailto:'.get_sub_field('email').'">'.get_sub_field('email')."</a>";}
+                                            ?>
+                                            </p>
+                                            <?php
+                                            $content = substr(get_sub_field('description'),0,80);
+                                            $content = substr($content,0,strrpos($content,' '));
+                                            $$content = preg_replace('/<span[^>]+\>/i', '', $content);
+                                            echo '<div class="desc_short">'.$content."...</div>";
+                                            ?>
+
+                                            <div class="allcontent"><?php echo get_sub_field('description'); ?></div>
+                                            <p class="read-more-button">More</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
 
                   <?php endwhile; endif; ?>
-                </div>    
-              </div>    
+
+              </div>
 
         </div>
       </div>
-    </div>        
+    </div>
     <?php get_template_part('template-parts/downloads'); ?>
 
 </div>

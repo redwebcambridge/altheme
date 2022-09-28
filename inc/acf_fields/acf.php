7df9d8679b940b45693b319c72aa860ecaf7e7fe
Andy Metcalfe <80961807+andy-metcalfe@users.noreply.github.com>
2022-09-28 15:42:12 +0100

    Afc fields added

---
+++ inc/acf_fields/acf.php
@@ -0,0 +1,25 @@
+<?php
+// all fields
+include_once('global.php');
+//about us 
+include_once('about-us.php');
+//contact
+include_once('contact.php');
+//contact
+include_once('course-content.php');
+//gallery
+include_once('gallery.php');
+//homepage
+include_once('homepage.php');
+//meet the staff
+include_once('meet-the-staff.php');
+//adult-learning-sports-categories
+include_once('adult-learning-sports-categories.php');
+//news
+include_once('news.php');
+//settings
+include_once('school-settings.php');
+// //Settings
+include_once('vacancies.php');
+
+
---
+++ inc/acf_fields/adult-learning-sports-categories.php
@@ -0,0 +1,57 @@
+<?php
+
+acf_add_local_field_group(array(
+	'key' => 'group_60c0e68bf1191',
+	'title' => 'Adult Learning and Sports Categories',
+	'fields' => array(
+		array(
+			'key' => 'field_60c0e69346d9e',
+			'label' => 'Image',
+			'name' => 'image',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'preview_size' => 'medium',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'taxonomy',
+				'operator' => '==',
+				'value' => 'adult-learning-category',
+			),
+		),
+		array(
+			array(
+				'param' => 'taxonomy',
+				'operator' => '==',
+				'value' => 'sports-centre-category',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => false,
+));

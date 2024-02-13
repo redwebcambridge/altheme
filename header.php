b579ae4c76a8c90a6f238b42e79706725e048240
Lewis Cornwell <56541433+lewiscornwell@users.noreply.github.com>
2024-02-13 17:31:21 +0000

    Opening Anglian Learning (logo) link in new tab

--- header.php
+++ header.php
@@ -211,7 +211,7 @@ if (!class_exists('ACF')) {
         </div>
         <!-- Anglian learning logo -->
         <div class="col-6 col-md-2 al-logo">
-          <a href="https://anglianlearning.org/" title="Anglian Learning ">
+          <a href="https://anglianlearning.org/" title="Anglian Learning" target="_blank">
             <img class="image-right img-fluid pt-2" src="https://anglianlearning.org/wp-content/themes/Anglian-Learning-Live/images/logo.png" alt="Anglian Learning" />
           </a>
         </div>

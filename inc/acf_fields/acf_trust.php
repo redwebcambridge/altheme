ef8d7a24bcedb9727d4be28a3b962b4697090672
Lewis Cornwell <lewis@redwebcambridge.com>
2024-09-05 16:28:25 +0100

    Al email alert settings

--- inc/acf_fields/acf_trust.php
+++ inc/acf_fields/acf_trust.php
@@ -1,4 +1,4 @@
 <?php
-// include_once('acf_trust_home.php');
 include_once('acf_trust_academies.php');
 include_once('acf_trust_central_team.php');
+include_once('acf_trust_settings.php');
---
+++ inc/acf_fields/acf_trust_settings.php
@@ -0,0 +1,47 @@
+<?php
+
+acf_add_local_field_group(array(
+	'key' => 'al_settings',
+	'title' => 'Anglian learning Settings',
+	'fields' => array(
+		array(
+			'key' => 'academy_news_emails',
+			'label' => 'Academy News Email Alerts',
+			'name' => 'academy_news_emails',
+			'type' => 'text',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'school-settings',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => array(
+		0 => 'permalink',
+		1 => 'the_content',
+		2 => 'excerpt',
+		3 => 'discussion',
+		4 => 'comments',
+		5 => 'revisions',
+		6 => 'slug',
+		7 => 'author',
+		8 => 'format',
+		9 => 'page_attributes',
+		10 => 'featured_image',
+		11 => 'categories',
+		12 => 'tags',
+		13 => 'send-trackbacks',
+	),
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+));

6b90d730a945310ed61b62c4ef2c9a6e564b197f
Lewis Cornwell <lewis@redwebcambridge.com>
2024-10-20 22:53:12 +0100

    Update vacancies.php

--- page-templates/vacancies.php
+++ page-templates/vacancies.php
@@ -13,15 +13,17 @@ get_header(); ?>
         <h2 class="heading-two"><?php echo get_field('sub_heading'); ?></h2>
         <div class="gradline"></div>
         <p class="body-text"><?php echo get_field('body_text'); ?></p>
-
-        <?php //echo the_content(); ?>
+        <a class="btn text-white" href="https://ce0976li.webitrent.com/ce0976li_webrecruitment/wrd/run/ETREC179GF.open?WVID=612290007I" target="_blank">Click here to view current vacancies advertised from 21 October 2024</a>
     </div>
 
 <?php if(get_field('school_id','option') != 'jfa') : ?>
 
     <div class="col-12">
+        <p class="mt-3">For current vacancies advertised prior to 21 October 2024 please see below.</p>
+
         <h2 class="heading-two mt-5">Current Vacancies</h2>
         <div class="gradline mb-5"></div>   
+        
     </div>
     <div class="col-12 mb-5">
         <!-- BEGIN MYNEWTERM API HTML: PLACE INSIDE PAGE BODY WHERE YOU WANT VACANCIES TO DISPLAY. PLEASE DO NOT CHANGE DIV ID (id="mnt-parent-container") !!! -->

60ce69dd109fff07aed5de3cf9f15695cfeb5ea0
Lewis Cornwell <lewis@redwebcambridge.com>
2025-06-11 17:03:15 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -345,7 +345,7 @@
                     <div class="row">
                         <div class="col">
                             Company registration number: 07564749
-                            <div class="w-100 pb-2 trust_footer_text">
+                            <div class="w-100 pb-4 trust_footer_text">
                                     <?php if ( get_field('school_id','option') != 'al' ) : ?>
                                         <?php if ( get_field('school_name','option') ) : echo get_field('school_name','option'); endif; ?>
                                         is operated by Anglian Learning, an exempt charitable company limited by guarantee and registered in England and Wales with company number 07564749. The registered office is at Bottisham Village College, Lode Road, Bottisham, Cambridge, CB25 9DL
